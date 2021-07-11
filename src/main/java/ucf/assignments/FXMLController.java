af517ac48380f8dc91c13860e519310b9906c8cc
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-07-11 14:39:27 -0400

    Commit one

--- src/main/java/ucf/assignments/FXMLController.java
+++ src/main/java/ucf/assignments/FXMLController.java
@@ -49,7 +49,7 @@ public class FXMLController {
         }
         else {
             todolistlist.getItems().addAll(Title.getText());
-            
+
             Title.setText("");
         }
     }
--- src/main/java/ucf/assignments/ListAddController.java
+++ src/main/java/ucf/assignments/ListAddController.java
@@ -1,14 +1,35 @@
-/*
- *  UCF COP3330 Summer 2021 Assignment 4 Solution
- *  Copyright 2021 Alex Goodwin
- */
-
 package ucf.assignments;
 
 import javafx.event.ActionEvent;
 
 public class ListAddController {
-    public void addListButtonClicked(ActionEvent actionEvent) {
-        //adds the items in the display to the items where they belong
+    public void addbuttonClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void editlistButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void displayincompleteButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void displaycompleteButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void displayAllbuttonClicked(ActionEvent actionEvent) {
+    }
+
+    public void exportlistButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void ExportalllistButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void importlistsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void checkboxClicked(ActionEvent actionEvent) {
     }
 }
